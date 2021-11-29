9a503f14020118e3ee162b5aaaaec14f85a18ee7
l33th <l33th@protonmail.ch>
2021-11-29 10:57:01 +0200

    machina voice

--- machina.py
+++ machina.py
@@ -1,13 +1,22 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
-
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'english_rp+f3')
+engine.say('Hello, I am Machina, your personal virtual assistant.')
+engine.say('What can I do for you?.')
+engine.runAndWait()
 try:
   with sr.Microphone() as source:
     print('listening...')
     voice = listener.listen(source)
     command = listener.recognize_google(voice)
-    print(command)
+    command = command.lowser()
+    if 'Machina' in command:
+
+      print(command)
 
 except:
     pass
