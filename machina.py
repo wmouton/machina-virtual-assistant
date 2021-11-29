5fadbefb0b6836bf3df5b0a5f47151c70ca979bb
l33th <l33th@protonmail.ch>
2021-11-29 11:08:59 +0200

    handle play command

--- machina.py
+++ machina.py
@@ -10,14 +10,26 @@ def talk(text):
   engine.say(text)
   engine.runAndWait()
 
-try:
-  with sr.Microphone() as source:
-    print('listening...')
-    voice = listener.listen(source)
-    command = listener.recognize_google(voice)
-    command = command.lowser()
-    if 'Machina' in command:
-      talk(command)
+def take_command():
+  try:
+    with sr.Microphone() as source:
+      print('listening...')
+      voice = listener.listen(source)
+      command = listener.recognize_google(voice)
+      command = command.lowser()
+      if 'machina' in command:
+        command = command.replace('machina', '')
+        print(command)
 
-except:
-    pass
+  except:
+      pass
+  return command
+
+def run_machina():
+  command = take_command()
+  print(command)
+  if 'play' in command:
+    talk('playing...')
+    print('playing...')
+
+run_machina()
