d6ec932b0f4f44ca525557a92f0fbcd5096fc1e0
l33th <l33th@protonmail.ch>
2021-11-29 10:21:27 +0200

    added voice recognition

--- machina.py
+++ machina.py
@@ -1,1 +1,13 @@
-print('Hello, I am Machina.  A virtual assistant created by l33th.')
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+  with sr.Microphone() as source:
+    print('listening...')
+    voice = listener.listen(source)
+    command = listener.recognize_google(voice)
+    print(command)
+
+except:
+    pass
